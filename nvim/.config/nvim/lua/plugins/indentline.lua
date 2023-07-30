06d5174a24df36b3c858b585b5735c6088007210
Kieran Burke <kieran314237@gmail.com>
2023-07-30 23:30:25 +0900

    maint: update indent colors

--- nvim/.config/nvim/lua/plugins/indentline.lua
+++ nvim/.config/nvim/lua/plugins/indentline.lua
@@ -2,8 +2,8 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   event = "BufEnter",
   config = function()
-    vim.cmd([[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]])
-    vim.cmd([[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]])
+    vim.cmd([[highlight IndentBlanklineIndent1 guibg=#1a1b26 gui=nocombine]])
+    vim.cmd([[highlight IndentBlanklineIndent2 guibg=#1e1f2e gui=nocombine]])
     vim.opt.list = true
     require("indent_blankline").setup({
       space_char_blankline = " ",
