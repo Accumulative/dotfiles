1e43ef9e0a79cc50685bcd6474fc4e314d68c49f
Kieran Burke <kieran314237@hotmail.co.uk>
2024-09-27 12:37:19 +0900

    switch to lazyvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,25 +1,2 @@
---Remap space as leader key
-vim.api.nvim_set_keymap("", "<Space>", "<Nop>", {})
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("user.options")
-require("lazy").setup("plugins")
-
-require("user.keymaps")
-require("user.lsp")
-require("user.autocommands")
-require("user.telescope")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
